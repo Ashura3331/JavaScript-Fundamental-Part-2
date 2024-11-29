16839ad284383bff89371de86df399e4f2445239
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-29 21:43:50 +0600

    new update

--- function.js
+++ function.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+function logger () { // first function that show output of this sentence.
+    console.log("My name is Ash");
+}
+
+
+// callilng // running// invoking function
+logger();
+logger();
+logger();
+
+
+ function fruitProcessor(apples, oranges) {// function taking with arguments and then return the value to function .
+    console.log(apples, oranges);
+     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+     return juice;
+ }
+
+
+ const appleJuice = fruitProcessor(3, 5);
+ console.log(appleJuice);
