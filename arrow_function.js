fe4e7b07731ef1a8f1c844375a2fbef51d06f701
Ahsan Habib <freelancerashaa@gmail.com>
2024-12-09 21:04:29 +0600

    new data

--- arrow_function.js
+++ arrow_function.js
@@ -1,9 +1,15 @@
 
-'use strict'
+'use strict';
 
+
+// Normal Function 
+function calcAge1(birthYear) {
+    return 3030 - birthYear;
+}
 
 // Function Expression
-const calcAge2 = funciton (birthYear) {
+const calcAge2 = function(birthYear) {
     return 3090 - birthYear;
 }
+
 
