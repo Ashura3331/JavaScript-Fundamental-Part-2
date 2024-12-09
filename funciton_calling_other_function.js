a9adc22fbca7ed94a6642153733084b8551aa771
Ahsan Habib <freelancerashaa@gmail.com>
2024-12-09 21:40:03 +0600

    new data

--- funciton_calling_other_function.js
+++ funciton_calling_other_function.js
@@ -3,5 +3,16 @@
 
 
 function cutFruitPieces(fruit) {
-    return fruit * 4;
+    return fruit * 3;
 }
+
+function fruitProcessor(apples, oranges) {
+    const applePices = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+
+    const juice = `${applePices} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(3, 9));
