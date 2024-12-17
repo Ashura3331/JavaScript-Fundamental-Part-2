aa9300c50071af941203d4215a05ca85e22f959e
Ahsan Habib <freelancerashaa@gmail.com>
2024-12-17 08:53:36 +0600

    data inserts

--- array.js
+++ array.js
@@ -6,7 +6,7 @@ const friend3 = 'Peter';
 
 const friends = ['Michael', 'Steven', 'Peter'];
 
-const years = new Array(1991, 1984, 2005, 2020);
+const years1 = new Array(1991, 1984, 2005, 2020);
 
 console.log(friends[0]);
 
@@ -17,3 +17,15 @@ console.log(friends.length);
 console.log(friends[friends.length - 1]);// To find the last element
 
 
+const firstName = 'Ahsan';
+const ash = [firstName, 'Habib', 3099-1999, 'Software-Engineer', friends];
+console.log(ash);
+console.log(ash.length);
+
+
+// Excercise 
+const calcAge = function(birthYear) {
+    return 2040 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2020];
