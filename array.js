6d70dabcac59aff76cef785cca90e195177f8279
Ahsan Habib <freelancerashaa@gmail.com>
2024-12-17 08:43:47 +0600

    data inserts

--- array.js
+++ array.js
@@ -4,3 +4,16 @@ const friend1 = 'Michael';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
 
+const friends = ['Michael', 'Steven', 'Peter'];
+
+const years = new Array(1991, 1984, 2005, 2020);
+
+console.log(friends[0]);
+
+console.log(friends[0]);
+
+console.log(friends.length);
+
+console.log(friends[friends.length - 1]);// To find the last element
+
+
