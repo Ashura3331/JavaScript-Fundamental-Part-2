82c7ecb2e472ae0c233a8e0461cadf019900ca3a
Ahsan Habib <freelancerashaa@gmail.com>
2024-12-19 22:47:53 +0600

    data unlock

--- array.js
+++ array.js
@@ -30,8 +30,8 @@ const calcAge = function(birthYear) {
 
 const years = [1990, 1967, 2002, 2018];
 
-// const age1 = calcAge(years[0]);
-// const age2 = calcAge(years[1]);
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
 const age3 = calcAge(years[years.length - 1]);
 console.log(age1, age2, age3);
 
